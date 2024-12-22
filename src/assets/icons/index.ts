d7a82f44a6e61227a06df9a40234a07b3c0587ae
Shagun Dubey <1202shagun@gmail.com>
2024-12-23 01:21:37 +0530

    update

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,23 @@
+import CreditCardIcon from './credit-card.svg';
+import EconometricsIcon from './econometrics.svg';
+import GlyphIcon from './Glyph.svg';
+import Logo from './Group.png';
+import HomeIcon from './home.svg';
+import InvestmentIcon from './investment.svg';
+import LoanIcon from './loan.svg';
+import SeriviceIcon from './service.svg';
+import SettingsIcon from './settings.svg';
+import UserIcon from './user.svg';
+
+export {
+  CreditCardIcon,
+  EconometricsIcon,
+  GlyphIcon,
+  LoanIcon,
+  HomeIcon,
+  InvestmentIcon,
+  Logo,
+  SeriviceIcon,
+  SettingsIcon,
+  UserIcon,
+};
